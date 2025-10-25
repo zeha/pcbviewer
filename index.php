50e375dcd8126f311d38d727bbc44a9c9c855cf4
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2025-10-25 18:37:02 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<style>
+body { margin: 0; background: black; }
+#sources { color: white; font-family: sans-serif; }
+</style>
+<script type="module" src="kicanvas.js"></script>
+<body>
+<?php
+/*
+$projects = [
+    pocket-reform/pocket-reform-wifi/pocket-reform-wifi.kicad_pro
+pocket-reform/pocket-reform-charger/pocket-reform-charger.kicad_pro
+pocket-reform/pocket-reform-motherboard/pocket-reform-motherboard.kicad_pro
+pocket-reform/pocket-reform-headphones/pocket-reform-headphones.kicad_pro
+pocket-reform/pocket-reform-keyboard-kailh-ortho/pocket-reform-keyboard-kailh-ortho.kicad_pro
+
+
+if ($_GET['project']) {
+
+}
+ */
+
+    <div id="sources">
+Other sources: XXX<br>
+    </div>
+<kicanvas-embed
+    controls="full"
+    controlslist="nodownload"
+    >
+    <kicanvas-source src="./pocket-reform/pocket-reform-motherboard/pocket-reform-motherboard.kicad_pcb"></kicanvas-source>
+</kicanvas-embed>
+</body>
